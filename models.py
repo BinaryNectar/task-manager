c65ae512271ffe71125574a460f78049f2c9f1fe
BinaryNectar <dariush.samari.tech@gmail.com>
2025-07-19 19:45:41 -0400

    Update models.py
    Create new "deadline" field for the table

--- models.py
+++ models.py
@@ -1,5 +1,5 @@
 from sqlalchemy.orm import DeclarativeBase
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Date, Integer, String, Boolean
 
 class Base(DeclarativeBase):
     """SQLAlchemy 2.0-style declarative base."""
@@ -10,11 +10,13 @@ class Task(Base):
 
     id = Column(Integer, primary_key=True)
     title = Column(String, nullable=False)
+    deadline = Column(Date, nullable=True)
     completed = Column(Boolean, default=False)
 
     def to_dict(self):
         return {
             "id": self.id,
             "title": self.title,
+            "deadline": self.deadline.isoformat() if self.deadline is not None else None,
             "completed": self.completed
         }
