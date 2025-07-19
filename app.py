8c6537801f47d89f39aee8dad542ac73c556dcdf
BinaryNectar <dariush.samari.tech@gmail.com>
2025-07-19 19:43:42 -0400

    Update app.py
    Enable application to utilize webpage

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 from routes import bp
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, static_folder="static", static_url_path="")
+
+    # serve index.html for the root URL
+    @app.route("/")
+    def serve_ui():
+        return send_from_directory(app.static_folder, "index.html")
+    
     app.register_blueprint(bp)
     return app
 
 if __name__ == '__main__':
     app = create_app()
     app.run(debug=True)
+
